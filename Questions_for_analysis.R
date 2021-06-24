5c257a94d702307f7a98e88bab80bc647908ab8b
Monserrat Garcia <monserratgarciar30@gmail.com>
2021-06-23 22:23:39 -0700

    Analysis Questions

---
+++ Questions_for_analysis.R
@@ -0,0 +1,15 @@
+# Questions for Analysis ####
+# 2021-06-23
+# Author: Monse Garcia
+
+#Question 1 ####
+#Sites relationship with oyster species
+#Example: Which sites are more prominent to bacteria or do more bacteria diversity in a specific sites. 
+
+
+#Question 2 ####
+#Look at peacrabs and sites. 
+
+
+
+#Question 3 ####
