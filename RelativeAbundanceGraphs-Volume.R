239a7010a180d4804a0dee103450f2d5c6e24425
Monserrat Garcia <monserratgarciar30@gmail.com>
2022-04-02 22:48:09 -0700

    Scaling Volume Column

--- RelativeAbundanceGraphs-Volume.R
+++ RelativeAbundanceGraphs-Volume.R
@@ -49,7 +49,7 @@ physeq_count18_posotuvol = subset_taxa(physeq_count18_posotuvol, Genus.x != "NA"
 # Change x to unique id or samples 
 plot_bar(physeq_count18_posotuvol, x= "UniqueID", fill= "Genus.x")+
   geom_bar(aes(color=Genus.x, fill = Genus.x), stat = "identity", position = "stack") +
-  facet_grid(~Bucket2, scales="free_x") +
+  facet_grid(~Bucket2, scales="free_x", space = "free_x") +
   scale_fill_manual(values = mycolors2) +
   scale_color_manual(values = mycolors2) +
   theme_bw() +
@@ -74,3 +74,42 @@ plot_bar(physeq_count18_posotu2, x= "UniqueID", fill= "Genus.x")+
         axis.line = element_line(color = "black"),
         axis.text.x = element_text(angle=90, hjust=1),
         text = element_text(size=9)) 
+
+
+####Scaling Volume Column 
+
+# 2017
+meta17_data <- read.csv("Data/meta17_data_update.csv")
+
+meta17_data <- meta17_data %>%
+  mutate(Volume_scale= scale(Volume_delta))
+
+meta17_data <- subset(meta17_data, select = -c(X.1, X, X.2, X))
+
+write.csv(meta17_data, file = "Data/meta17_data_update.csv")
+
+meta17_data_lowscale <- meta17_data %>% 
+  filter(Volume_scale > 0)
+
+meta17_data_highscale <- meta17_data %>%  # Lost 8 values due to the differences in measurements not having values 
+  filter(Volume_scale < 0)
+
+
+#2018
+
+meta_gen18_data <- read.csv("Data/metagenetics_data18.csv")
+
+meta_gen18_data <- meta_gen18_data %>%
+  mutate(Volume_scale= scale(Volume_delta))
+
+meta_gen18_data <- subset(meta_gen18_data, select = -c(X.1, X))
+
+write.csv(meta_gen18_data, file = "Data/metagenetics_data18.csv")
+
+meta_gen18_data_highscale <- meta_gen18_data %>% 
+  filter(Volume_scale > 0)
+
+meta_gen18_data_lowscale <- meta_gen18_data %>%   # Lost 25 values due to the differences in measurements not having values 
+  filter(Volume_scale < 0)
+
+
