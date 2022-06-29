25c4673bdb9517fb9bbf01bafd2f91a7524ddd86
Monserrat Garcia <monserratgarciar30@gmail.com>
2022-06-29 12:48:51 -0400

    Results script for the measurement analysis.

---
+++ Measurment Analysis- Results.R
@@ -0,0 +1,57 @@
+# Measurment Analysis- Results ####
+# 2022-06-29
+# Author: Monse Garcia
+
+# Loading packages
+require(phyloseq)
+require(ggplot2)
+require(data.table)
+require(RColorBrewer)
+library("ggpubr")
+library(dplyr)
+library(tidyr)
+require(DESeq2)
+library(metacoder)
+library(ggtree)
+library(Rcpp)
+
+#### 1.Phyloseq Analysis ith scaled volume 
+
+physeq_highsc17 <- readRDS("Data/physeq_highsc17.rds")
+
+taxa_scaled17 = tax_table(physeq_count18_length)
+
+des_highsc17 <- phyloseq_to_deseq2(physeq_highsc17, ~ Volume_scale)
+des_highsc17 <- DESeq(des_highsc17, test="Wald", fitType = "parametric")
+resultsNames(des_highsc17)
+# Intercept , volume_scale
+
+results_highsc17 <- results(des_highsc17, name = "Volume_scale")
+significant_highsc17 <- results_highsc17[which(results_highsc17$padj <0.05), ]
+dim(significant_highsc17)
+# 121, 6
+
+#### Positive OTUs
+
+significant_highsc17_pos <- significant_highsc17[significant_highsc17$log2FoldChange>0,]
+dim(significant_highsc17_pos)
+# 41, 6
+
+
+
+#### Negative 
+significant_highsc17_neg <- significant_highsc17[significant_highsc17$log2FoldChange<0, ]
+dim(significant_highsc17_neg)
+# 80, 6
+
+#### 2.Measurements taxa
+# This section covers the significant OTUs associated with measurements; length, width, height.
+
+
+
+
+
+
+
+
+
