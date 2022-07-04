84cef34d59507a7c3f62acc2923b53ebaaee1c15
Monserrat Garcia <monserratgarciar30@gmail.com>
2022-07-04 09:47:23 -0400

    Finishing subsetting taxa

--- Measurment Analysis- Results.R
+++ Measurment Analysis- Results.R
@@ -1,4 +1,4 @@
-# Measurment Analysis- Results ####
+# Measurement Analysis- Results ####
 # 2022-06-29
 # Author: Monse Garcia
 
@@ -15,7 +15,7 @@ library(metacoder)
 library(ggtree)
 library(Rcpp)
 
-#### 1.Phyloseq Analysis ith scaled volume 
+#### 1.Phyloseq Analysis with scaled volume 
 
 physeq_highsc17 <- readRDS("Data/physeq_highsc17.rds")
 
@@ -31,12 +31,18 @@ significant_highsc17 <- results_highsc17[which(results_highsc17$padj <0.05), ]
 dim(significant_highsc17)
 # 121, 6
 
+sub_significant_highsc17 <- subset_taxa(prune_taxa(rownames(significant_highsc17), physeq_highsc17))
+df_significant_highsc17 <- as.data.frame(tax_table(sub_significant_highsc17))
+write.table(df_significant_highsc17, file = "Data/Significant OTU's for Scaled Volume", quote = FALSE, sep = ",", col.names = T)
+
+
 #### Positive OTUs
 
 significant_highsc17_pos <- significant_highsc17[significant_highsc17$log2FoldChange>0,]
 dim(significant_highsc17_pos)
 # 41, 6
 
+sub_significant_highsc17_pos
 
 
 #### Negative 
@@ -45,8 +51,16 @@ dim(significant_highsc17_neg)
 # 80, 6
 
 #### 2.Measurements taxa
-# This section covers the significant OTUs associated with measurements; length, width, height.
+# This section covers the significant OTUs associated with measurements; length, width, height. NOTE: These are not scaled nor are they the volume.  
 
+# Taxa- Phylum 
+# 1. Proteobacteria 
+# 2. Chloroflexi
+# 3. Planctomycetota 
+# 4. Desulfobacteria 
+# 5. Cyanobacteria
+# 6. Modulibacteria 
+# 7. SAR324 clade (marine group B)
 
 
 
