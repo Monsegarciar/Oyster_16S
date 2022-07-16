aa0f94ebfbfa51043fe01afa33e7a2627acd964f
Monserrat Garcia <monserratgarciar30@gmail.com>
2022-07-16 12:33:58 -0400

    Samples

--- Measurment Analysis- Results.R
+++ Measurment Analysis- Results.R
@@ -29,11 +29,11 @@ resultsNames(des_highsc17)
 results_highsc17 <- results(des_highsc17, name = "Volume_scale")
 significant_highsc17 <- results_highsc17[which(results_highsc17$padj <0.05), ]
 dim(significant_highsc17)
-# 121, 6
+# 67, 6
 
 sub_significant_highsc17 <- subset_taxa(prune_taxa(rownames(significant_highsc17), physeq_highsc17))
 df_significant_highsc17 <- as.data.frame(tax_table(sub_significant_highsc17))
-write.table(df_significant_highsc17, file = "Data/Significant OTU's for Scaled Volume", quote = FALSE, sep = ",", col.names = T)
+write.table(df_significant_highsc17, file = "Data/Significant OTU's for Scaled Volume 2017.csv", quote = FALSE, sep = ",", col.names = T)
 
 
 #### Positive OTUs
@@ -50,10 +50,40 @@ significant_highsc17_neg <- significant_highsc17[significant_highsc17$log2FoldCh
 dim(significant_highsc17_neg)
 # 80, 6
 
+# 2018 
+physeq_highsc18 <- readRDS("Data/physeq_highsc18.rds")
+
+taxa_scaled17 = tax_table(physeq_count18_length)
+
+des_highsc18 <- phyloseq_to_deseq2(physeq_highsc18, ~ Volume_scale)
+des_highsc18 <- DESeq(des_highsc18, test="Wald", fitType = "parametric")
+resultsNames(des_highsc18)
+# Intercept , volume_scale
+
+results_highsc18 <- results(des_highsc18, name = "Volume_scale")
+significant_highsc18 <- results_highsc18[which(results_highsc18$padj <0.05), ]
+dim(significant_highsc18)
+# 51, 6
+
+sub_significant_highsc18 <- subset_taxa(prune_taxa(rownames(significant_highsc18), physeq_highsc18))
+df_significant_highsc18 <- as.data.frame(tax_table(sub_significant_highsc18))
+write.table(df_significant_highsc18, file = "Data/Significant OTU's for Scaled Volume 2018.csv", quote = FALSE, sep = ",", col.names = T)
+
+
+#### Positive OTUs
+
+significant_highsc18_pos <- significant_highsc18[significant_highsc18$log2FoldChange>0,]
+dim(significant_highsc18_pos)
+
+#### Negative OTUs
+
+significant_highsc18_neg <- significant_highsc18[significant_highsc18$log2FoldChange<0, ]
+dim(significant_highsc18_neg)
+
 #### 2.Measurements taxa
 # This section covers the significant OTUs associated with measurements; length, width, height. NOTE: These are not scaled nor are they the volume.  
 
-# Taxa- Phylum 
+# Taxa- Class
 # 1. Proteobacteria 
 # 2. Chloroflexi
 # 3. Planctomycetota 
@@ -61,6 +91,8 @@ dim(significant_highsc17_neg)
 # 5. Cyanobacteria
 # 6. Modulibacteria 
 # 7. SAR324 clade (marine group B)
+
+# Any papers on such taxa and relationships with oysters
 
 
 
