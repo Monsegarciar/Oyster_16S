11e0fbfea52a4f447c499ac4c7b92d44e3035a05
Monsegarciar <monserratgarciar30@gmail.com>
2023-04-18 18:20:13 -0700

    Update on graphs

--- Growth Analysis.R
+++ Growth Analysis.R
@@ -117,6 +117,7 @@ log2 <- merge(sss17_2, sigtab_17_vol,by ="row.names")
 log2fold <- subset(log2, select = -c(Kingdom.x, Phylum.x, Class.x, Order.x, Genus.y.x, Species.x, Family.x, Genus.x.x, log2FoldChange.y))
 
 write.csv(log2fold, file = "Data/log2fold2017.csv")
+
 ###### Turning into Phyloseq Object #####
 
 meta17_data <- read.csv("Data/meta17_data_update.csv")
@@ -177,16 +178,20 @@ dd <- as.data.frame(significant17_pos)
 sub_significant17_pos <- subset_taxa(prune_taxa(rownames(significant17_pos), physeq_count17))
 sub_significant17_pos
 
+physeq17_v <- readRDS("Data/physeq_sitexvol17.rds")
+physeq17_v
+sam_v17 <- as.data.frame(sample_data(physeq17_v))
 heatmap17 = parse_phyloseq(physeq17_v)
 
 heatmap17 %>%
   heat_tree(node_label = gsub(pattern = "\\[|\\]", replacement = "", taxon_names),
-            node_size = n_obs,
-            node_color = n_obs,
+            node_size = n_obs,node_label_size_range = c(0.03, 0.04),
+            node_color = n_obs,node_size_range = c(0.03, .1),
             layout = "davidson-harel", initial_layout = "reingold-tilford", node_color_axis_label = "Number of Obs")
 
 #2018 Volume and Bucket ####
 physeq_count18 = subset_samples(physeq_count18, Volume_scale != "NA")
+physeq_count18= subset_samples(physeq_count18, Species2.x !="IR")
 des_count18 <- phyloseq_to_deseq2(physeq_count18, ~ Volume_scale + Bucket2)
 
 gm_mean = function(row) if (all(row == 0)) 0 else exp(mean(log(row[row != 0])))
@@ -237,12 +242,11 @@ df <- as.data.frame(otu_table(physeq_count18))
 
 # Loading Data
 meta_gen18_data <- read.csv("Data/metagenetics_data18.csv")
-
 Run123_taxa <- fread("Data/Run123_taxa_complete - Copy.csv")
 
 #Changing row names in "Run123_taxa"
 Run123_taxa$V1=NULL
-rownames(table18)= Run123_taxa$V2
+rownames(Run123_taxa)= Run123_taxa$V2
 head(rownames(Run123_taxa))
 
 #Changing row names in meta_gen18 data
@@ -301,18 +305,16 @@ Phy.ord <- ordinate(physeq_sig18, "NMDS", "bray")
 plot_ordination(physeq_sig18, Phy.ord, type = "biplot", color = "Volume_scale", shape = "Bucket2", title = "biplot")
 
 physeq18_v <- readRDS("Data/physeq_volxbuck18.rds")
-
 heatmap18_vol = parse_phyloseq(physeq18_v)
 
 heatmap18_vol %>%
   heat_tree(node_label = gsub(pattern = "\\[|\\]", replacement = "", taxon_names),
-            node_size = n_obs,
-            node_color = n_obs,
+            node_size = n_obs,node_label_size_range = c(0.05, 0.06),
+            node_color = n_obs,node_size_range = c(0.05, .1), node_size_interval = range(n_obs, na.rm = TRUE, finite = TRUE),
             layout = "davidson-harel", initial_layout = "reingold-tilford", node_color_axis_label = "Number of Obs")
 
 taxtable18 = as.data.frame(tax_table(physeq18_v))
 # Weight #####
-sd <- sample_data(physeq_count17)
 
 physeq_weight = subset_samples(physeq_count17, Weight_delta != "NA")
 des_weight17 <- phyloseq_to_deseq2(physeq_weight, ~ Weight_delta + Site.x)
@@ -346,6 +348,7 @@ phy_weight17 = prune_taxa(sfweight_17, physeq_weight17)
 # 2018 
 
 physeq_weight18 = subset_samples(physeq_count18, Weight_delta !="NA")
+physeq_weight18 = subset_samples(physeq_count18, Species2.x !="IR")
 
 des_weight18 <- phyloseq_to_deseq2(physeq_weight18, ~ Weight_delta + Bucket2)
 
@@ -442,7 +445,7 @@ plot_bar(phys, "Order", fill = "Phylum", facet_grid = ~Description) +
   ylab("Percentage of Sequences") 
 
 heatmap = parse_phyloseq(physeq_buckxweight18)
-
+samda18 <- as.data.frame(sample_data(physeq_buckxweight18))
 phy_weight_sig18
 
 ggplot(sigtab_weight18, aes(x=Phylum, y=rownames(taxtable_BuckxWeigh), color=Phylum)) + geom_point(size=6) + 
@@ -450,8 +453,8 @@ ggplot(sigtab_weight18, aes(x=Phylum, y=rownames(taxtable_BuckxWeigh), color=Phy
 
 heatmap %>%
   heat_tree(node_label = gsub(pattern = "\\[|\\]", replacement = "", taxon_names),
-            node_size = n_obs,
-            node_color = n_obs,
+            node_size = n_obs,node_label_size_range = c(0.03, 0.04),
+            node_color = n_obs,node_size_range = c(0.03, .1),
             layout = "davidson-harel", initial_layout = "reingold-tilford", node_color_axis_label = "Number of Obs")
   
 
@@ -551,11 +554,11 @@ taxtable_voldelxbuck18 <- as.data.frame(tax_table(physeq_sigvol18))
 
 heatmap18 = parse_phyloseq(physeq_sigvol18)
 
-heatmap18 = parse_phyloseq(taxtable_sitexvol17)
+heatmap18 = parse_phyloseq(as.data.frame(log2fold18_weight))
 
 heatmap18 %>%
   heat_tree(node_label = gsub(pattern = "\\[|\\]", replacement = "", taxon_names),
-            node_size = n_obs,
+            node_size = n_obs,node_label_size_range=c(0.02,0.02),
             node_color = n_obs,
             layout = "davidson-harel", initial_layout = "reingold-tilford", node_color_axis_label = "Number of Obs")
 write.csv(taxtable_voldelxbuck18, file = "Data/taxtable_voldelxbuck18.csv")
@@ -592,8 +595,17 @@ x = tapply(log2fold18_vol$log2FoldChange, log2fold18_vol$Genus, function(x) max(
 x = sort(x, TRUE)
 log2fold18_vol$Genus = factor(as.character(log2fold18_vol$Genus), levels=names(x))
 
-ggplot(log2fold18_vol, aes(x=Genus, y=log2FoldChange, color=Genus)) + 
-  theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5))+ labs(title = "Log2FoldChange Volume 2018")+ ylab("Log2FoldChange")+geom_bar(stat="identity", aes(fill = Genus))
+Genus_vol18 <- length(unique(log2fold18_vol$Genus))
+Genus_pal_v18 <- colorRampPalette(brewer.pal(5,"Set1"))
+
+ggplot(log2fold18_vol, aes(x=Row.names, y=log2FoldChange, fill=Genus)) + theme_classic() +geom_hline(yintercept = 0)+
+  theme(axis.text.x = element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), panel.border=element_rect(color = "black",fill = NA, size=1),
+        axis.title = element_text(face = "bold"),text = element_text(size = 14) )+ 
+  ylab("Log2FoldChange")+xlab("Genus")+geom_bar(stat="identity")+
+  scale_x_discrete(labels= log2fold18_vol$Genus)+
+  geom_text(aes(label=sprintf(log2FoldChange,fmt = "%0.2f"), fontface="bold"), vjust=-0.5, color="black",size=4.0)+
+  scale_fill_manual(values = Genus_pal_v18(Genus_vol18))
 
 ##### 2018 Weight ####
 
@@ -601,8 +613,18 @@ x = tapply(log2fold18_weight$log2FoldChange, log2fold18_weight$Genus, function(x
 x = sort(x, TRUE)
 log2fold18_weight$Genus = factor(as.character(log2fold18_weight$Genus), levels=names(x))
 
-ggplot(log2fold18_weight, aes(x=Genus, y=log2FoldChange, color=Genus)) + 
-  theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5))+ labs(title = "Log2FoldChange Weight 2018")+ylab("Log2FoldChange")+geom_bar(stat="identity", aes(fill = Genus))
+Genus_weigh18 <- length(unique(log2fold18_weight$Genus))
+Genus_pal_w18 <- colorRampPalette(brewer.pal(7,"Set1"))
+
+ggplot(log2fold18_weight, aes(x=Row.names, y=log2FoldChange,fill=Genus))+theme_classic() +geom_hline(yintercept = 0)+
+  theme(axis.text.x = element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),panel.border=element_rect(color = "black",fill = NA, size=0.5),
+        axis.title = element_text(face = "bold"),text = element_text(size = 14))+
+  ylab("Log2FoldChange")+
+  scale_x_discrete(labels= log2fold18_weight$Genus)+
+  geom_bar(stat="identity")+xlab("Genus")+
+  geom_text(aes(label=sprintf(log2FoldChange,fmt = "%0.2f"), fontface="bold"),vjust=-0.5, color="black",size=3.5)+
+  scale_fill_manual(values = Genus_pal_w18(Genus_weigh18))
 
 ##### 2017 ####
 
@@ -610,15 +632,233 @@ x = tapply(log2fold2017$log2FoldChange.x, log2fold2017$Genus, function(x) max(x)
 x = sort(x, TRUE)
 log2fold2017$Genus = factor(as.character(log2fold2017$Genus), levels=names(x))
 
-ggplot(log2fold2017, aes(x=Genus, y=log2FoldChange.x, color=Genus)) + 
-  theme(axis.text.x = element_text(angle = -90, hjust = 0, vjust=0.5)) + labs(title = "Log2FoldChange Volume 2017") +  ylab("Log2FoldChange")+geom_bar(stat="identity", aes(fill = Genus))
+Genus_vol17 <- length(unique(log2fold2017$Genus))
+Genus_pal <- colorRampPalette(brewer.pal(7,"Set1"))
+
+ggplot(log2fold2017, aes(x=Row.names, y=log2FoldChange.x,fill=Genus))+geom_bar(stat="identity")+  theme_classic() +
+  theme(axis.text.x = element_blank(),
+        panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border=element_rect(color = "black",fill = NA, size=1),
+        axis.title = element_text(face = "bold"),text = element_text(size = 14) )+ 
+  ylab("Log2FoldChange")+xlab("Genus")+
+ scale_x_discrete(labels= log2fold2017$Genus) + 
+  geom_bar(stat="identity")+
+  geom_text(aes(label=sprintf(log2FoldChange.x,fmt = "%0.2f"), fontface="bold"), vjust=-0.5, color="black",size=4.0)+
+  scale_fill_manual(values = Genus_pal(Genus_vol17))
+
+#https://www.datanovia.com/en/blog/ggplot-theme-background-color-and-grids/#:~:text=To%20remove%20a%20particular%20panel,grid.
 
 
+# Mussels for 2018 #### 
+
+physeq_count18 = subset_samples(physeq_count18, Volume_scale != "NA")
+physeq_count18= subset_samples(physeq_count18, Species2.x !="CV")
+des_count18 <- phyloseq_to_deseq2(physeq_count18, ~ Volume_scale + Bucket2)
+
+gm_mean = function(row) if (all(row == 0)) 0 else exp(mean(log(row[row != 0])))
+geoMeans = apply(OTU_count18, 2, gm_mean)
+deseq18 = estimateSizeFactors(des_count18, geoMeans=geoMeans, locfunc=shorth)
+deseq18_v = DESeq(deseq18, test="Wald", fitType="parametric")
+
+results18 <- results(deseq18_v, name = "Volume_scale")
+significant18 <- results18[which(results18$padj <0.05), ]
+sigtab18 = cbind(as(significant18, "data.frame"), as(tax_table(physeq_count18)[rownames(significant18), ], "matrix"))
+
+# Creating DESEq results into a tax table ####
+sigtab_18_muss <- subset(sigtab18, select = -c(baseMean, 
+                                              lfcSE, stat, pvalue, padj))
 
 
+# Turning it into a phyloseq ####
+physeq_count18
+taxa18 <- as.matrix(sigtab_18_muss)
+taxa_muss18 <- tax_table(taxa18)
+physeq18 = subset_taxa(prune_taxa(rownames(taxa_muss18), physeq_count18)) 
+physeq_count18
+physeq18
+
+  
+physeq_muss18 = tax_filter(physeq18, min_prevalence = 0.3, min_sample_abundance = 1)
+physeq_muss18
+
+#physeq_sig18 = genefilter(physeq18, filterfun_sample(function(x) x > 0, A=0.3*nsamples(physeq18)))
+
+muss_18 <- genefilter_sample(physeq18, filterfun_sample(function(x) x > 0), A=0.3*nsamples(physeq18))
 
 
+physeq18_mu = prune_taxa(muss_18, physeq18)
+physeq18_mu
+# 6 taxa 
 
+mu18 <- as.data.frame(tax_table(physeq18_mu))
+mv18 <- merge(mu18, sigtab_18_muss, by ='row.names', all = TRUE)
+mvv18 <- subset.data.frame(mv18, Kingdom.x != "NA")
+mvv18_2 <- subset(mvv18, select = -c(Kingdom.x,Phylum.x, Class.x, Order.x, Family.x, Genus.x.x, Genus.y.x,Species.x))
+write.csv(mvv18_2, file = "Data/log2fold2018_volxmussels.csv")
+
+otu_mu18 <- otu_table(physeq18_mu)
+otu_vmu18 <- otu_mu18 +1
+View(otu_vmu18)
+
+# Loading Data
+meta_gen18_data <- read.csv("Data/metagenetics_data18.csv")
+Run123_taxa <- fread("Data/Run123_taxa_complete - Copy.csv")
+
+#Changing row names in "Run123_taxa"
+Run123_taxa$V1=NULL
+rownames(Run123_taxa)= Run123_taxa$V2
+head(rownames(Run123_taxa))
+
+#Changing row names in meta_gen18 data
+rownames(meta_gen18_data)= meta_gen18_data$UniqueID 
+head(rownames(meta_gen18_data))
+
+#Setting taxmat and otumat
+taxmat18=Run123_taxa
+taxmat18=Run123_taxa[-c(1)]
+
+#Converting to matrix
+tax_matrix18=as.matrix(taxmat18, rownames = "V2")
+colnames(tax_matrix18) <- c("Kingdom", "Phylum", "Class", "Order", "Family", 
+                            "Genus.x", "Genus.y", "Species")
+
+#Setting OTU, TAX, and SAMP
+OTU18= otu_vmu18
+
+TAX18= tax_table(tax_matrix18)
+
+SAMP18= sample_data(meta_gen18_data)
+
+physeq_mus18 = phyloseq(OTU18, TAX18, SAMP18)
+physeq_mus18
+
+saveRDS(physeq_mus18, "Data/physeq_volxmussels18.rds")
+
+taxtablemuss_18 <- as.data.frame(tax_table(physeq_mus18))
+
+taxtablemuss18 <- as.data.frame(tax_table(physeq_mus18))
+
+write.csv(taxtablemuss18, file = "Data/taxtable_volxmussels18.csv")
+
+
+# 2018 Mussels Graph and Heatmap ####
+# Loading Data #
+
+log2fold18_volxmuss <- read.csv("Data/log2fold2018_volxmussels.csv")
+
+Genus_volmuss18 <- length(unique(log2fold18_volxmuss$Genus))
+Genus_pal_vm18 <- colorRampPalette(brewer.pal(7,"Set1"))
+
+ggplot(log2fold18_volxmuss, aes(x=Row.names, y=log2FoldChange,fill=Genus))+theme_classic() +geom_hline(yintercept = 0)+
+  theme(axis.text.x = element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),panel.border=element_rect(color = "black",fill = NA, size=0.5),
+        axis.title = element_text(face = "bold"),text = element_text(size = 14))+
+  ylab("Log2FoldChange")+
+  scale_x_discrete(labels= log2fold18_volxmuss$Genus)+
+  geom_bar(stat="identity")+xlab("OTU")+
+  geom_text(aes(label=sprintf(log2FoldChange,fmt = "%0.2f"), fontface="bold"),vjust=1.3, color="black",size=3.5)+
+  scale_fill_manual(values = Genus_pal_vm18(Genus_volmuss18))
+
+heatmap_muss = parse_phyloseq(physeq_mus18)
+
+heatmap_muss %>%
+  heat_tree(node_label = gsub(pattern = "\\[|\\]", replacement = "", taxon_names),
+            node_size = n_obs,node_label_size_range = c(0.03, 0.04),
+            node_color = n_obs,node_size_range = c(0.03, .1),
+            layout = "davidson-harel", initial_layout = "reingold-tilford", node_color_axis_label = "Number of Obs")
+
+#### Weight #####
+physeq_weight18 = subset_samples(physeq_count18, Weight_delta !="NA")
+physeq_weight18 = subset_samples(physeq_weight18, Species2.x !="CV")
+
+
+des_weight18 <- phyloseq_to_deseq2(physeq_weight18, ~ Weight_delta + Bucket2)
+
+gm_mean = function(row) if (all(row == 0)) 0 else exp(mean(log(row[row != 0])))
+geoMeans = apply(OTU_count18, 2, gm_mean)
+deseq_weight18 = estimateSizeFactors(des_weight18, geoMeans=geoMeans, locfunc=shorth)
+des_weight18 <- DESeq(deseq_weight18, test="Wald", fitType = "parametric")
+
+results_weight18 <- results(des_weight18, name = "Weight_delta")
+significant_weight18 <- results_weight18[which(results_weight18$padj <0.05), ]
+sigtab_weight18 = cbind(as(significant_weight18, "data.frame"), as(tax_table(physeq_count18)[rownames(significant_weight18), ], "matrix"))
+
+sigtab_muss_weight <- subset(sigtab_weight18, select = -c(baseMean, 
+                                                        lfcSE, stat, pvalue, padj))
+
+physeq_count18
+taxa_wm <- as.matrix(sigtab_muss_weight)
+taxa_weightmuss <- tax_table(taxa_wm)
+physeq_weightmuss = subset_taxa(prune_taxa(rownames(taxa_weightmuss), physeq_count18))
+physeq_count18
+physeq_weightmuss
+
+wm <- as.data.frame(tax_table(physeq_weight_muss))
+wm18 <- merge(wm, sigtab_muss_weight, by ='row.names', all = TRUE)
+wmm18 <- subset.data.frame(wm18, Kingdom.x != "NA")
+wmmm18_2 <- subset(wmm18, select = -c(Kingdom.x,Phylum.x, Class.x, Order.x, Family.x, Genus.x.x, Genus.y.x,Species.x))
+write.csv(wmmm18_2, file = "Data/log2fold2018_mussels.csv")
+
+# Weight and bucket= 2730, 6 taxa 
+phys_weightmuss = tax_filter(physeq_weightmuss, min_prevalence = 0.3, min_sample_abundance = 1)
+phys_weightmuss
+
+muweight_18 <- genefilter_sample(physeq_weightmuss, filterfun_sample(function(x) x > 0), A=0.3*nsamples(physeq_weightmuss))
+# 1302, 6
+
+phy_weightmu = prune_taxa(muweight_18, physeq_weightmuss)
+phy_weightmu
+#7, 8
+
+otu_weight18 <- otu_table(phy_weightmu)
+otu_w18 <- otu_weight18 +1
+View(otu_w18)
+
+#Setting OTU, TAX, and SAMP
+OTU18= otu_w18
+
+TAX18= tax_table(tax_matrix18)
+
+SAMP18= sample_data(meta_gen18_data)
+
+phy_weight_muss = phyloseq(OTU18, TAX18, SAMP18)
+phy_weight_muss
+
+saveRDS(phy_weight_muss, "Data/physeq_musselsxweight18.rds")
+
+physeq_weight_muss<- readRDS("Data/physeq_musselsxweight18.rds")
+
+taxtable_weightmuss <- as.data.frame(tax_table(phy_weight_muss))
+
+taxtable_mussxWeigh <- as.data.frame(tax_table(phy_weight_muss))
+
+write.csv(taxtable_BuckxWeigh, file = "Data/taxtable_Bucket&Weight.csv")
+
+
+#### Graphs for Weight #####
+
+log2fold_mussels <- read.csv("Data/log2fold2018_mussels.csv")
+
+Genus_weighmuss <- length(unique(log2fold_mussels$Genus))
+Genus_pal_wmuss <- colorRampPalette(brewer.pal(7,"Set1"))
+
+ggplot(log2fold_mussels, aes(x=Row.names, y=log2FoldChange,fill=Genus))+theme_classic() +geom_hline(yintercept = 0)+
+  theme(axis.text.x = element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),panel.border=element_rect(color = "black",fill = NA, size=0.5),
+        axis.title = element_text(face = "bold"),text = element_text(size = 14))+
+  ylab("Log2FoldChange")+
+  scale_x_discrete(labels= log2fold_mussels$Genus)+
+  geom_bar(stat="identity")+xlab("OTU")+
+  geom_text(aes(label=sprintf(log2FoldChange,fmt = "%0.2f"), fontface="bold"),vjust=-0.5, color="black",size=3.5)+
+  scale_fill_manual(values = Genus_pal_wmuss(Genus_weighmuss))
+
+
+heatmap_muss = parse_phyloseq(physeq_weight_muss)
+
+heatmap_muss %>%
+  heat_tree(node_label = gsub(pattern = "\\[|\\]", replacement = "", taxon_names),
+            node_size = n_obs,node_label_size_range = c(0.03, 0.04),
+            node_color = n_obs,node_size_range = c(0.03, .1),
+            layout = "davidson-harel", initial_layout = "reingold-tilford", node_color_axis_label = "Number of Obs")
 
 
 
